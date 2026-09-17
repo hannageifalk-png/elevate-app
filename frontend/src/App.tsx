f187ae15d9e5ccb15e8e759f2cd777443f9300de
Lynn Wigardt <lynn.wigardt@hotmail.com>
2026-09-17 14:30:25 +0200

    Add HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
